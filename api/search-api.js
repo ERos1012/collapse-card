c67f4ee1faae4f9214a493d09fa44b746dc4a781
msanyal7 <113386966+msanyal7@users.noreply.github.com>
2023-04-08 16:40:07 -0400

    idk

--- api/search-api.js
+++ api/search-api.js
@@ -1,55 +1,77 @@
 export default async function handler(request, res) {
-  
-    const search = [
-      {
-        badgeHeader: "Badge Header",
-        badgeName: "Badge Name",
-        badgeUrl: "Badge Url",
-        badgeDescription: "Badge Description",
-        badgeImage: "Badge Name",
-        badgeCreator: "Badge Creator",
-        timeToComplete: "Time To Complete",
-        stepsName: "Steps Name",
-        stepsDescription: "Steps Description",
-        stepsTime: "Steps Time",
-      },
-      {
-        badgeHeader: "More Header",
-        badgeName: "More Name",
-        badgeUrl: "More Url",
-        badgeDescription: "More Description",
-        badgeImage: "More Image",
-        badgeCreator: "More Creator",
-        timeToComplete: "More To Complete",
-        stepsName: "More Name",
-        stepsDescription: "More Description",
-        stepsTime: "More Time",
-      },
-      {
-        badgeHeader: "Badge Header",
-        badgeName: "Badge Name",
-        badgeUrl: "Badge Url",
-        badgeDescription: "Badge Description",
-        badgeImage: "Badge Image",
-        badgeCreator: "Badge Creator",
-        timeToComplete: "Time To Complete",
-        stepsName: "Steps Name",
-        stepsDescription: "Steps Description",
-        stepsTime: "Steps Time",
-      },
-    ];
-  
-    res.setHeader("Cache-Control", "max-age=0, s-maxage=1800");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-    res.json(search);
-  }
+  const search = request.query.search || "";
+ 
+  var badges = [
+    {
+      badgeHeader: "Badge Header",
+      badgeName: "Badge Name",
+      badgeUrl: "Badge Url",
+      badgeDescription: "Badge Description",
+      badgeImage: "Badge Image",
+      badgeCreator: "Badge Creator",
+      timeToComplete: "Time To Complete",
+      stepsName: "Steps Name",
+      stepsDescription: "Steps Description",
+      stepsTime: "Steps Time",
+    },
+    {
+      badgeHeader: "More Header",
+      badgeName: "More Name",
+      badgeUrl: "More Url",
+      badgeDescription: "More Description",
+      badgeImage: "More Image",
+      badgeCreator: "More Creator",
+      timeToComplete: "More To Complete",
+      stepsName: "More Name",
+      stepsDescription: "More Description",
+      stepsTime: "More Time",
+    },
+    {
+      badgeHeader: "Badge Header",
+      badgeName: "Badge Name",
+      badgeUrl: "Badge Url",
+      badgeDescription: "Badge Description",
+      badgeImage: "Badge Image",
+      badgeCreator: "Badge Creator",
+      timeToComplete: "Time To Complete",
+      stepsName: "Steps Name",
+      stepsDescription: "Steps Description",
+      stepsTime: "Steps Time",
+    },
+  ];
+
+  badges.map((badge) => {
+    badge.index =
+      badge.badgeName.toLowerCase() +
+      " " +
+      badge.badgeHeader.toLowerCase() +
+      " " +
+      badge.badgeCreator.toLowerCase() +
+      " " +
+      badge.badgeDescription.toLowerCase() +
+      " " +
+      badge.stepsName.toLowerCase() +
+      " " +
+      badge.stepsTime.toLowerCase() +
+      " " +
+      badge.timeToComplete.toLowerCase();
+  });
+
+  badges = badges.filter((badge) => {
+    return badge.index.indexOf(search.toLowerCase()) > -1;
+  });
+
+  res.setHeader("Cache-Control", "max-age=0, s-maxage=1800");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  res.json(badges);
+}
   
