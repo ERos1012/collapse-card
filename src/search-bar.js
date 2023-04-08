2ac7ce526e5100520ca03288cf5d03ad3ec8b4cd
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-04-08 16:03:03 -0400

    added CSS to searchbar

--- src/search-bar.js
+++ src/search-bar.js
@@ -1,45 +1,75 @@
 import { LitElement, html, css } from "lit";
-import '@lrnwebcomponents/simple-icon/simple-icon.js';
+import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
 class searchBar extends LitElement {
   static get properties() {
     return {
       value: { type: String },
-    }
+    };
   }
 
   static get styles() {
     return css`
-    :host {
-      display: block;
-    }
-    simple-icon {
-      display: inline-block;
-      --simple-icon-height: 40px;
-      --simple-icon-width: 40px;
-    }
+      :host {
+        display: block;
+      }
+      simple-icon {
+        display: inline-block;
+        --simple-icon-height: 30px;
+        --simple-icon-width: 30px;
+      }
+      .wrapper {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        margin: 20px;
+      }
+      #searchbar {
+        width: 100%;
+        max-width: 500px;
+        filter: drop-shadow(0px 2px 3px #383838);
+      }
+      input {
+        font-size: 20px;
+        font-weight: bold;
+        border: none;
+      }
+      input:focus {
+        outline: 1px solid grey;
+      }
     `;
   }
 
   constructor() {
     super();
-    this.value = '';
+    this.value = "";
   }
 
   render() {
     return html`
-      <simple-icon icon="icons:search"></simple-icon>
-      <input type="text" value="${this.value}" @input="${this._handleInput}" />
+      <div class="wrapper">
+        <simple-icon icon="icons:search"></simple-icon>
+        <input
+          type="text"
+          id="searchbar"
+          placeholder="Search Content, Topics, and People"
+          value="${this.value}"
+          @input="${this._handleInput}"
+        />
+      </div>
     `;
   }
   _handleInput(e) {
     this.value = e.target.value;
-    this.dispatchEvent(new CustomEvent('value-changed', {
-      detail: {
-        value: this.value,
-      }
-    }));
+    this.dispatchEvent(
+      new CustomEvent("value-changed", {
+        detail: {
+          value: this.value,
+        },
+      })
+    );
     console.log(this.value);
   }
 }
